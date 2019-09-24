6d2b93a2cf20804af14580257849ecbf22eeafc4
Ruan Carvalho <ruancarvalho@gmail.com>
2019-09-23 21:54:42 -0400

    feat(search): added message to input more letters

--- src/ui/search/cmp-search.js
+++ src/ui/search/cmp-search.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Search = ({ onChange, value }) => (
-  <input
-    type="text"
-    onChange={onChange}
-    value={value}
-    placeholder="Enter pokemon name..."
-    autoFocus
-  />
+  <Fragment>
+    <input
+      type="text"
+      onChange={onChange}
+      value={value}
+      placeholder="Enter a pokemon name..."
+      autoFocus
+    />
+    {value.length > 0 && value.length < 3 && (
+      <div>
+        <small>Please, type more letters...</small>
+      </div>
+    )}
+  </Fragment>
 )
 
 export default Search
